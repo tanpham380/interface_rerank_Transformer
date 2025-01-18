c7b65a4c3b39314b5e0af7b24088456f844e381f
tanpham380 <tanpham380>
2025-01-18 09:47:49 +0700

    Refactor document handling and update request body structure

--- test.py
+++ test.py
@@ -33,23 +33,27 @@
 import requests
 
 url = 'http://localhost:8001/rerank'
-documents = [
-    "Quĩ uỷ thác đầu tư (tiếng Anh: Unit Investment Trusts; viết tắt: UIT) là một công ty đầu tư mua hoặc nắm giữ một danh mục đầu tư cố định",
-    "FPT software là một công ty phần mềm của tập đoàn FPT, chuyên cung cấp các giải pháp phần mềm cho các doanh nghiệp và tổ chức trên thế giới.",
-    "Trường Đại học Kinh tế – Luật (tiếng Anh: University of Economics and Law – UEL) là trường đại học đào tạo và nghiên cứu khối ngành kinh tế, kinh doanh và luật hàng đầu Việt Nam.",
-    "Trường Đại học Công nghệ Thông tin có tên tiếng Anh là University of Information Technology (viết tắt là UIT) là thành viên của Đại học Quốc Gia TP.HCM.",
-    "Đại học FPT là một trường đại học tư thục tại Việt Nam, thành lập vào năm 2006"
-]
+# documents = [
+#     "Quĩ uỷ thác đầu tư (tiếng Anh: Unit Investment Trusts; viết tắt: UIT) là một công ty đầu tư mua hoặc nắm giữ một danh mục đầu tư cố định",
+#     "FPT software là một công ty phần mềm của tập đoàn FPT, chuyên cung cấp các giải pháp phần mềm cho các doanh nghiệp và tổ chức trên thế giới.",
+#     "Trường Đại học Kinh tế – Luật (tiếng Anh: University of Economics and Law – UEL) là trường đại học đào tạo và nghiên cứu khối ngành kinh tế, kinh doanh và luật hàng đầu Việt Nam.",
+#     "Trường Đại học Công nghệ Thông tin có tên tiếng Anh là University of Information Technology (viết tắt là UIT) là thành viên của Đại học Quốc Gia TP.HCM.",
+#     "Đại học FPT là một trường đại học tư thục tại Việt Nam, thành lập vào năm 2006"
+# ]
 query = " Tập đoàn FPT ?"
-req_body = {'query': query, 'documents': documents}
+property_text = """
+FPT Software là công ty phần mềm hàng đầu thuộc tập đoàn FPT, cung cấp các giải pháp phần mềm trên toàn thế giới.
+"""
+
+req_body = {'query': query, 'property': property_text}
 res = requests.post(url, json=req_body)
 resBody = res.json()
 
 # Tìm tài liệu có điểm số cao nhất
-highest_score_doc = max(resBody['scores'], key=lambda x: x['score'])
+# highest_score_doc = max(resBody['scores'], key=lambda x: x['score'])
 
 print(resBody)
 
-print('Highest score doc:')
+# print('Highest score doc:')
 
-print(highest_score_doc)
+# print(highest_score_doc)
